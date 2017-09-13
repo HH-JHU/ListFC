01763f0df1f94d380ad06ed949fb46a48f0c7861
HH-JHU <31872659+HH-JHU@users.noreply.github.com>
2017-09-12 20:43:25 -0400

    List FC in GDB
    List all the Feature Classes in Geodatabase using ArcPy

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
 # Author:  
 # Date:
-# Purpose:  
+# Purpose:  To List all Feature Classes in GDB
 
 
 # Import ArcPy
@@ -8,14 +8,14 @@ import arcpy
  
 
 # Set the workspace
- 
+arcpy.env.workspace = r"C:\Users\Aaron\Desktop\School\JHU Master's Program\2017\Programming_in_GIS\Module2\Assignment2\Newark.gdb"
 
 # List all of the feature classes in the Newark geodatabase 
-
+featureClasses= arcpy.ListFeatureClasses()
 
 # Iterate through all the feature classes and print the name of each feature class 
-
+for fc in featureClasses:
+    print(fc)
 
 # Print "Complete"
-
-
+print "Complete"
